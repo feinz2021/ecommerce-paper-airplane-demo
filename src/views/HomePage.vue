830cbfdeada4cd6d0608abb93320f9f24482c2dd
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-07-04 21:42:57 +0800

    update

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,20 @@
 <template>
-homepage
-<h5>testing UBUNTU Ubuntu</h5>
+  homepage
+  <div class="container">
+    <a class="btn z-depth-0 rc">button</a>
+    <div class="card-panel z-depth-0 green rc">asd</div>
+
+    <h5>testing UBUNTU Ubuntu</h5>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-export default {
-}
+export default {};
 </script>
+<style>
+.rc {
+  border-radius: 10px !important;
+}
+</style>
