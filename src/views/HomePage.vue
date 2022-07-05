2eec8611b358a9e611d6e46dc9aa32debd401444
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-07-05 11:46:38 +0800

    pd8

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,14 +4,28 @@
     <a class="btn z-depth-0 rc">button</a>
     <div class="card-panel z-depth-0 green rc">asd</div>
 
-    <h5>testing UBUNTU Ubuntu</h5>
+    <h5>testing UBUNTU {{ user }}</h5>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-export default {};
+export default {
+  data() {
+    return {
+      //   user: "",
+    };
+  },
+  mounted() {
+    this.$store.commit("user", "test user");
+  },
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
+  },
+};
 </script>
 <style>
 .rc {
