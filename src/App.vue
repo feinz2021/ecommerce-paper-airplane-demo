9d0692f6f58448320bb41c74718f2a3a4c141752
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-07-08 21:22:39 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <router-view/>
+  <NavBar />
+  <router-view />
 </template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+export default {
+  components: { NavBar },
+};
+</script>
 
 <style>
 @font-face {
   font-family: "Ubuntu";
   src: local("Ubuntu"),
-   url(./fonts/Ubuntu/Ubuntu-Regular.ttf) format("truetype");
+    url(./fonts/Ubuntu/Ubuntu-Regular.ttf) format("truetype");
 }
-html, body {
-  font-family: 'Ubuntu', sans-serif;
+html,
+body {
+  font-family: "Ubuntu", sans-serif;
 }
 #app {
-  font-family: 'Ubuntu', sans-serif;
+  font-family: "Ubuntu", sans-serif;
 }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,98 +1,29 @@
-<<<<<<< HEAD
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img
-          src="https://bulma.io/images/bulma-logo.png"
-          width="112"
-          height="28"
-        />
-      </a>
-
-      <a
-        role="button"
-        class="navbar-burger"
-        :class="mobileMenuOpen"
-        @click="menuMobile()"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo hide-on-med-and-down">Logo</a>
+      <a href="#!" class="brand-logo right hide-on-large-only">Logo</a>
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"
+        ><i class="material-icons">menu</i></a
       >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+      </ul>
 
-    <div id="navbarBasicExample" class="navbar-menu" :class="mobileMenuOpen">
-      <div class="navbar-start">
-        <a class="navbar-item"> Home </a>
-
-        <a class="navbar-item"> Documentation </a>
-
-        <div class="navbar-item has-dropdown">
-          <a class="navbar-link" @click="menuDropdown()"> More </a>
-
-          <div class="navbar-dropdown" :style="mobileDropdown">
-            <a class="navbar-item"> About </a>
-            <a class="navbar-item"> Jobs </a>
-            <a class="navbar-item"> Contact </a>
-            <hr class="navbar-divider" />
-            <a class="navbar-item"> Report an issue </a>
-          </div>
-        </div>
-      </div>
-
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-            <a class="button is-primary">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light"> Log in </a>
-          </div>
-        </div>
+      <div class="input-field" style="width: 60%; margin-left: 20%">
+        <input id="search" type="search" />
+        <label class="label-icon" for="search"
+          ><i class="material-icons">search</i></label
+        >
+        <i class="material-icons">close</i>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      mobileMenuOpen: "",
-      mobileDropdown: "",
-    };
-  },
-  methods: {
-    menuMobile() {
-      if (this.mobileMenuOpen === "") {
-        this.mobileMenuOpen = "is-active";
-        this.mobileDropdown = "display: none";
-      } else this.mobileMenuOpen = "";
-    },
-    menuDropdown() {
-      if (this.mobileMenuOpen === "is-active") {
-        if (this.mobileDropdown === "display: none")
-          this.mobileDropdown = "display: block";
-        else if (this.mobileDropdown === "")
-          this.mobileDropdown = "display: block";
-        else this.mobileDropdown = "display: none";
-      } else {
-        if (this.mobileDropdown === "display: none")
-          this.mobileDropdown = "display: block";
-        else if (this.mobileDropdown === "")
-          this.mobileDropdown = "display: block";
-        else this.mobileDropdown = "display: none";
-      }
-    },
-  },
-};
+export default {};
 </script>
 
 <style>
 </style>
-=======
->>>>>>> parent of 3828a7c (changed to bulma-css)
