b0ddbe92e7e508ad49dabbce6ba36165a26586b6
feinz2021 <84484716+feinz2021@users.noreply.github.com>
2022-07-12 23:37:37 +0800

    update

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container">
     <div style="margin-top: 2%"></div>
+
+    <!-- item list -->
     <div class="row">
       <div v-for="tes in test" :key="tes" class="col s6 m4 l3">
         <div
-          class="card-panel z-depth-0 brown lighten-5 rc"
+          class="card-panel z-depth-0 brown lighten-5 rc fp"
           style="height: 300px"
           @click="gotoProductDetails(tes)"
         >
@@ -12,6 +14,27 @@
         </div>
       </div>
     </div>
+
+    <!-- pagination -->
+    <ul class="pagination center">
+      <li :class="currentPage === 1 ? disableNextPrev : enableNextPrev">
+        <a href="#!"><i class="material-icons">chevron_left</i></a>
+      </li>
+      <span v-for="(num, index) in test" :key="num">
+        <li
+          :class="
+            activePage === false ? paginationInactivePage : paginationActivePage
+          "
+        >
+          <a href="#!"> {{ index + 1 }}</a>
+        </li>
+      </span>
+      <li
+        :class="currentPage === test.length ? disableNextPrev : enableNextPrev"
+      >
+        <a href="#!"><i class="material-icons">chevron_right</i></a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -21,8 +44,17 @@
 export default {
   data() {
     return {
+      activePage: true,
+      currentPage: 1,
       //   user: "",
-      test: [1, 2, 3, 4, 5, 6],
+      test: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21,
+      ],
+      paginationActivePage: "active brown lighten-1",
+      paginationInactivePage: "waves-effect",
+      disableNextPrev: "disabled",
+      enableNextPrev: "waves-effect",
     };
   },
   mounted() {
@@ -33,16 +65,22 @@ export default {
       return this.$store.state.user;
     },
   },
-  methods:{
-    gotoProductDetails(id){
+  methods: {
+    gotoProductDetails(id) {
       sessionStorage.setItem("productId", id);
       this.$router.push("/productdetails");
-    }
-  }
+    },
+    clickCallback(pageNum) {
+      console.log(pageNum);
+    },
+  },
 };
 </script>
 <style>
 .rc {
   border-radius: 10px !important;
+}
+.fp {
+  cursor: pointer;
 }
 </style>
--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -1,7 +1,6 @@
 <template>
-  product details
-  <h5>{{ productId }}</h5>
   <div class="container">
+    <div style="margin-top: 2%"></div>
     <!-- horizontal card, large screen -->
     <div class="card horizontal z-depth-0 hide-on-med-and-down">
       <div class="card-image">
@@ -10,12 +9,20 @@
       <div class="card-stacked brown lighten-5 rc">
         <div class="card-content">
           <p>
-            I am a very simple card. I am good at containing small bits of
-            information.
+            product details <br />
+            {{ productId }}
           </p>
         </div>
         <div class="card-action brown lighten-5 rc">
-          <a href="#">This is a link</a>
+          <div class="center">
+            <a
+              @click="$router.go(-1)"
+              id="backBtn"
+              class="btn btn-flat waves-effect wave-dark rc fp"
+              style="color: #795548; width: 100%; height: 100%"
+              >Back<i class="material-icons-round right">arrow_back_ios</i></a
+            >
+          </div>
         </div>
       </div>
     </div>
@@ -33,7 +40,15 @@
         </p>
       </div>
       <div class="card-action brown lighten-5 rc">
-        <a href="#">This is a link</a>
+        <div class="center">
+          <a
+            @click="$router.go(-1)"
+            id="backBtn"
+            class="btn btn-flat waves-effect wave-dark rc fp"
+            style="color: #795548; width: 100%; height: 100%"
+            >Back<i class="material-icons-round right">arrow_back_ios</i></a
+          >
+        </div>
       </div>
     </div>
   </div>
@@ -55,5 +70,11 @@ export default {
 <style>
 .rc {
   border-radius: 10px !important;
+}
+.fp {
+  cursor: pointer;
+}
+#backBtn:hover {
+  background-color: transparent !important;
 }
 </style>
